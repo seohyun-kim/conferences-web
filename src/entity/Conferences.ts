9e1f24c354bbd275ecbe033a42f63f42196aa1c6
seohyun-kim <selenium@dgist.ac.kr>
2024-04-01 22:37:50 +0900

    fix : DB 엔티티 초기값 에러 해결

--- src/entity/Conferences.ts
+++ src/entity/Conferences.ts
@@ -3,20 +3,21 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class Conference {
   @PrimaryGeneratedColumn()
-  id: number;
+  id?: number;
 
   @Column()
-  name: string;
+  name?: string;
 
   @Column('datetime')
-  abstract_due: Date;
+  abstract_due?: Date;
 
   @Column('datetime')
-  full_paper_due: Date;
+  full_paper_due?: Date;
 
   @Column()
-  location: string;
+  location?: string;
 
   @Column('datetime')
-  conference_date: Date;
+  conference_date?: Date;
 }
+

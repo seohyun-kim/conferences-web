967ea7617bb99f41c27702d1dab8c2c074b5274a
seohyun-kim <selenium@dgist.ac.kr>
2024-04-01 23:27:49 +0900

    feat : 미들웨어 설정

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import conferenceRoutes from './routes/conferenceRoutes';
+import path from 'path'; // path 모듈을 import합니다.
 
 createConnection().then(async connection => {
   const app = express();
@@ -11,6 +12,9 @@ createConnection().then(async connection => {
   app.use(bodyParser.json());
 
   app.use('/api/conferences', conferenceRoutes);
+
+  // 정적 파일 제공 설정
+  app.use(express.static(path.join(__dirname, '../public')));
 
   app.listen(3000, () => {
     console.log('Server running on port 3000');
